9a77a2fd25df8dbd53d8209162c157de0bd7e1d4
dsotolongo <dsotolongo@twitter.com>
2015-10-11 19:55:54 -0700

    [t] confirm quadratic function works; impact_time broken

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,25 @@
+
 use std::ops::{Add, Mul, Sub};
 
-type Float = f32;
-
-struct Time(Float);
+pub type CustomFloat = f32;
+const SIGNIFICAND: i32 = 24;
 
 #[derive(Debug)]
-struct Vector((Float, Float));
+pub struct Time(CustomFloat);
+
+#[derive(Debug)]
+pub struct Vector((CustomFloat, CustomFloat));
+
+impl Vector {
+  fn norm2(&self) -> CustomFloat {
+    let &Vector((x1, x2)) = self;
+    x1 * x1 + x2 * x2
+  }
+}
 
 impl<'l> Mul for &'l Vector {
-  type Output = Float;
-  fn mul(self, rhs: &Vector) -> Float {
+  type Output = CustomFloat;
+  fn mul(self, rhs: &Vector) -> CustomFloat {
     let &Vector(v1) = self;
     let &Vector(v2) = rhs;
     v1.0 * v2.0 + v1.1 * v2.1
@@ -35,16 +45,63 @@ impl<'l> Sub for &'l Vector {
 }
 
 #[derive(Debug)]
-struct Particle {
+pub struct Particle {
   x: Vector,
   v: Vector,
-  r: Float,
-  m: Float,
+  r: CustomFloat,
+  m: CustomFloat,
+}
+
+/// Computes solutions to the quadratic formula:
+/// ( -b +/- sqrt(b^2 - 4ac) ) / 2a
+/// The smaller solution is always on the left.
+/// Returns None for imaginary results.
+///
+/// # Example
+/// ```
+/// use particles::quadratic_formula;
+/// let (s1, s2) = quadratic_formula(1., 0., -1.).unwrap();
+/// assert!((s1 + 1.).abs() < 1e-10);
+/// assert!((s2 - 1.).abs() < 1e-10);
+/// ```
+pub fn quadratic_formula(a: CustomFloat, b: CustomFloat, c: CustomFloat) -> Option<(CustomFloat, CustomFloat)> {
+  // account for floating error: we can't have degenerate solutions come up imaginary
+  let b2 = b * b + (b / 2.0_f32.powi(-SIGNIFICAND + 1));
+  let ac = 4. * a * c;
+
+  if ( b2 < ac ) { None } // imaginary result
+  else {
+    let a2 = 2. * a;
+    let fst = - b / a2;
+    let snd = ((b2 - ac).sqrt() / a2).abs();
+    Some((fst - snd, fst + snd))
+  }
 }
 
 impl Particle {
-  fn tt_impact(&self, other: &Particle) -> Time {
-    Time(0.0)
+  fn impact_time(&self, other: &Particle) -> Option<Time> {
+    // solves for t:
+    // | self.x - self.x + (self.v - other.v) * t | = self.r + other.r
+    let dv = &(&self.v - &other.v);
+    let dx = &(&self.x - &other.x);
+    let sr = self.r + other.r;
+
+    // quadratic formula for t:
+    // |dv|^2 t^2 + 2 (dx * dv) t + |dx|^2 - sr^2 = 0
+    let a = dv.norm2();
+    let b = 2. * ( dx * dv );
+    let c = dx.norm2() - sr.powi(2);
+
+    match quadratic_formula(a, b, c) {
+      Some((less, _)) if less >= 0. => Some(Time(less)),
+      Some((_, more)) if more <= 0. => None,
+      None => None, // particles will never impact
+      Some(solns) => // less < 0 && more > 0 <=> particles overlap
+        unreachable!(
+          "impact_time found overlapping particles:\n{:?}\n{:?}\n{:?}",
+          solns, self, other
+        ),
+    }
   }
 
   fn bounce(&self, other: &Particle) -> (Particle, Particle) {
@@ -71,19 +128,20 @@ trait Box<P> {
 }
 
 fn main() {
-  let v1 = Particle {
-    x: Vector((1., 1.)),
-    v: Vector((1., 1.)),
+  let p1 = Particle {
+    x: Vector((-2., 0.)),
+    v: Vector((1., 0.)),
     r: 1.,
     m: 1.
   };
 
-  let v2 = Particle {
-    x: Vector((1., 1.)),
-    v: Vector((1., 1.)),
+  let p2 = Particle {
+    x: Vector((2., 0.)),
+    v: Vector((-1., 0.)),
     r: 1.,
     m: 1.
   };
-  println!("v1: {:?}", v1);
-  println!("delta: {:?}", &v1.x - &v2.x);
+  println!("p1: {:?}", p1);
+  println!("delta: {:?}", &p1.x - &p2.x);
+  println!("impact_time: {:?}", p1.impact_time(&p2));
 }
