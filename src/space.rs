09e98a15b36ac8150b3055a09afc4479351e43c5
dsotolongo <dsotolongo@twitter.com>
2015-10-14 01:16:05 -0700

    [w] begin drafting BoundedBoxVec

--- src/space.rs
+++ src/space.rs
@@ -1,8 +1,14 @@
 use super::particle::Particle;
 
-pub trait Box {
+pub trait Space {
   fn create(initial: Vec<Particle>) -> Self;
   fn next_collision(&self) -> (&Particle, &Particle);
   fn update<'l>(&'l mut self, &p1: Particle, &p2: Particle) -> &'l mut Self;
+}
+
+pub struct BoundedBoxVec {
+  pub origin: Vector,
+  pub corner: Vector,
+  particles: Vec<Particle>
 }
 
