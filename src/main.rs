645865a971d2bc732a3bacef9f9de6d56bf50188
dsotolongo <dsotolongo@twitter.com>
2015-10-22 00:05:15 -0700

    [w] create a demo of unbounded space

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,34 @@
 extern crate particles;
+extern crate rand;
 
 use particles::*;
+use rand::{thread_rng};
 
 fn main() {
-  let p1 = Particle {
-    x: Vector((-2., 0.)),
-    v: Vector((1., 0.)),
-    r: 1.,
-    m: 1.
+  let max_particle = Particle {
+    x: Vector((15., 15.)),
+    v: Vector((1., 1.)),
+    m: 1.,
+    r: 1.
   };
+  let min_particle = Particle {
+    x: Vector((-15., -15.)),
+    v: Vector((-1., -1.)),
+    m: 1.,
+    r: 1.
+  };
+  let mut rng = thread_rng();
 
-  let p2 = Particle {
-    x: Vector((2., 0.)),
-    v: Vector((-1., 0.)),
-    r: 1.,
-    m: 1.
-  };
-  println!("p1: {:?}", p1);
-  println!("delta: {:?}", &p1.x - &p2.x);
-  println!("impact_time: {:?}", p1.impact_time(&p2));
+  let particles = (1..10)
+    .map( move |_| BoundedRand::rand(&mut rng, min_particle, max_particle))
+    .collect::<Vec<_>>();
+
+  let mut space = SpaceVec::new(particles);
+  let mut i = 0;
+
+  println!("starting: {:?}", space);
+  while i < 10 && space.iterate() {
+    println!("{:?}", space);
+  }
+  println!("ending");
 }
